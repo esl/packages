1510cc26c145f56b16aede3e67e157486e71c608
Diego Albarado <27455020+dalbarado@users.noreply.github.com>
2023-06-21 06:16:12 -0300

    Update file_list_creator.py
    added pagination

--- file_list_creator.py
+++ file_list_creator.py
@@ -37,9 +37,6 @@ s3_client = boto3.client('s3')
 # S3 bucket name
 bucket_name = 'binaries2.erlang-solutions.com'
 
-# Get the list of objects in the bucket
-response = s3_client.list_objects(Bucket=bucket_name)
-
 # Dictionary to store the final JSON structure for Elixir
 elixir_json_data = {
     "tabs": [],
@@ -64,61 +61,67 @@ mongooseim_json_data = {
     }
 }
 
-# Iterate over the objects
-for obj in response['Contents']:
-    filename = obj['Key']
+# Paginate over the objects in the bucket
+paginator = s3_client.get_paginator('list_objects')
+page_iterator = paginator.paginate(Bucket=bucket_name)
 
-    # Extract information from the filename
-    file_info = extract_info_from_filename(filename)
+for page in page_iterator:
+    if 'Contents' in page:
+        for obj in page['Contents']:
+            filename = obj['Key']
 
-    if file_info:
-        tab_name, full_os_name = filename.split("/")[-2:]
+            # Extract information from the filename
+            file_info = extract_info_from_filename(filename)
 
-        # Split the full_os_name into its parts
-        os_parts = full_os_name.split("~")
+            if file_info:
+                tab_name, full_os_name = filename.split("/")[-2:]
 
-        # Extract the os_name and version from os_parts
-        os_name = os_parts[1].split("_")[0] + " " + os_parts[2]  # Concatenate OS name and version
-        os_version = os_parts[2]
+                # Split the full_os_name into its parts
+                os_parts = full_os_name.split("~")
 
-        # Find the appropriate JSON data dictionary based on the tab name
-        json_data = None
-        if "elixir" in filename:
-            json_data = elixir_json_data
-        elif "esl-erlang" in filename:
-            json_data = erlang_json_data
-        elif "mongooseim" in filename:
-            json_data = mongooseim_json_data
+                # Extract the os_name and version from os_parts
+                os_name_file = os_parts[1].split("_")[0]  # Extract the OS name for the file
+                os_version = os_parts[2]  # Extract the OS version
+                os_name_with_version = os_name_file + " " + os_version  # Concatenate OS name and version
 
-        if json_data:
-            # Check if a tab with the OS name already exists
-            existing_tab = next((tab for tab in json_data["tabs"] if tab["name"] == os_name), None)
-            if existing_tab:
-                existing_flavour = next((flavour for flavour in existing_tab["flavours"] if flavour["name"] == "main"), None)
-                if existing_flavour:
-                    existing_flavour["packages"].append(file_info)
-                else:
-                    existing_tab["flavours"].append({
-                        "name": "main",
-                        "packages": [file_info],
-                        "header": "",
-                        "footer": ""
-                    })
-            else:
-                json_data["tabs"].append({
-                    "name": os_name,
-                    "caption": os_name.capitalize(),
-                    "header": "",
-                    "footer": "",
-                    "flavours": [
-                        {
-                            "name": "main",
-                            "packages": [file_info],
+                # Find the appropriate JSON data dictionary based on the tab name
+                json_data = None
+                if "elixir" in filename:
+                    json_data = elixir_json_data
+                elif "esl-erlang" in filename:
+                    json_data = erlang_json_data
+                elif "mongooseim" in filename:
+                    json_data = mongooseim_json_data
+
+                if json_data:
+                    # Check if a tab with the OS name already exists
+                    existing_tab = next((tab for tab in json_data["tabs"] if tab["name"] == os_name_with_version), None)
+                    if existing_tab:
+                        existing_flavour = next((flavour for flavour in existing_tab["flavours"] if flavour["name"] == "main"), None)
+                        if existing_flavour:
+                            existing_flavour["packages"].append(file_info)
+                        else:
+                            existing_tab["flavours"].append({
+                                "name": "main",
+                                "packages": [file_info],
+                                "header": "",
+                                "footer": ""
+                            })
+                    else:
+                        json_data["tabs"].append({
+                            "name": os_name_with_version,
+                            "caption": os_name_with_version.capitalize(),
                             "header": "",
-                            "footer": ""
-                        }
-                    ]
-                })
+                            "footer": "",
+                            "flavours": [
+                                {
+                                    "name": "main",
+                                    "packages": [file_info],
+                                    "header": "",
+                                    "footer": ""
+                                }
+                            ]
+                        })
 
 # Write separate JSON files for Elixir, esl-erlang, and MongooseIM
 with open('elixir_packages.json', 'w') as json_file:
